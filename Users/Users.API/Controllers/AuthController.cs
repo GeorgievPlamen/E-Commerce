463f4530e7ec1db672b851a3bbc817f59f9a04c6
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-04-01 07:39:03 +0300

    users controller

---
+++ Users/Users.API/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Users.Core.DTO;
+using Users.Core.ServiceContracts;
+
+namespace Users.API.Controllers;
+
+[Route("api/[controler]")]
+[ApiController]
+public class AuthController(IUsersService usersService) : ControllerBase
+{
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterRequest registerRequest)
+    {
+        if (registerRequest is null)
+            return BadRequest("Invalid");
+
+        var response = await usersService.Register(registerRequest);
+
+        if (response is null || !response.Success)
+            return BadRequest(response);
+
+        return Ok(response);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginRequest loginRequest)
+    {
+        if (loginRequest is null)
+            return BadRequest("Invalid");
+
+        var response = await usersService.Login(loginRequest);
+
+        if (response is null || !response.Success)
+            return Unauthorized(response);
+
+        return Ok(response);
+    }
+}
