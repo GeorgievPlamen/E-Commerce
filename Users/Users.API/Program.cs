23260c7db9d43f3f8485e2306b85552d6a6341e1
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-04-02 07:53:50 +0300

    swagger

--- Users/Users.API/Program.cs
+++ Users/Users.API/Program.cs
@@ -16,11 +16,25 @@ builder.Services.AddControllers().AddJsonOptions(opt =>
 });
 builder.Services.AddAutoMapper(Assembly.GetAssembly(typeof(ApplicationUserMappingProfile)));
 builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyMethod();
+        builder.AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
 app.UseExceptionHandlingMiddleware();
 app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
