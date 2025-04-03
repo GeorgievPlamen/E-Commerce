8efc5cab64f0988503497c75e10f2b99da068e95
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-04-03 07:50:09 +0300

    Implement products service

--- Products/Products.BLL/ServiceContracts/IProductsService.cs
+++ Products/Products.BLL/ServiceContracts/IProductsService.cs
@@ -1,13 +1,14 @@
 using System.Linq.Expressions;
 using Products.BLL.DTO;
+using Products.DAL.Entities;
 
 namespace Products.BLL.ServiceContracts;
 
 public interface IProductsService
 {
     Task<List<ProductResponse?>> GetProducts();
-    Task<List<ProductResponse?>> GetProductsByCondition(Expression<Func<ProductResponse, bool>> expression);
-    Task<ProductResponse?> GetProductByCondition(Expression<Func<ProductResponse, bool>> expression);
+    Task<List<ProductResponse?>> GetProductsByCondition(Expression<Func<Product, bool>> expression);
+    Task<ProductResponse?> GetProductByCondition(Expression<Func<Product, bool>> expression);
     Task<ProductResponse?> AddProduct(ProductAddRequest request);
     Task<ProductResponse?> UpdateProduct(ProductUpdateRequest request);
     Task<bool> DeleteProduct(Guid productId);
---
+++ Products/Products.BLL/Services/ProductsService.cs
@@ -0,0 +1,100 @@
+using System.Linq.Expressions;
+using AutoMapper;
+using FluentValidation;
+using Microsoft.AspNetCore.Builder;
+using Products.BLL.DTO;
+using Products.BLL.ServiceContracts;
+using Products.DAL.Entities;
+using Products.DAL.RepositoryContracts;
+
+namespace Products.BLL.Services;
+
+public class ProductsService(
+    IProductsRepository productsRepository,
+    IMapper mapper,
+    IValidator<ProductAddRequest> addValidator,
+    IValidator<ProductUpdateRequest> updateValidator) : IProductsService
+{
+    public async Task<ProductResponse?> AddProduct(ProductAddRequest request)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        var validationResult = await addValidator.ValidateAsync(request);
+
+        if (!validationResult.IsValid)
+        {
+            var errors = string.Join(",", validationResult.Errors.Select(x => x.ErrorMessage));
+
+            throw new ArgumentException(errors);
+        }
+
+        var product = mapper.Map<Product>(request);
+
+        var result = await productsRepository.AddProduct(product);
+
+        var response = mapper.Map<ProductResponse>(result);
+
+        return response;
+    }
+
+    public async Task<bool> DeleteProduct(Guid productId)
+    {
+        var product = await productsRepository.GetProductByCondition(x => x.ProductID == productId);
+
+        if (product is null)
+            return false;
+
+        var result = await productsRepository.DeleteProduct(product.ProductID);
+
+        return result;
+    }
+
+    public async Task<ProductResponse?> GetProductByCondition(Expression<Func<Product, bool>> expression)
+    {
+        var result = await productsRepository.GetProductByCondition(expression);
+
+        var response = mapper.Map<ProductResponse>(result);
+
+        return response;
+    }
+
+    public async Task<List<ProductResponse?>> GetProducts()
+    {
+        var result = await productsRepository.GetProducts();
+
+        var response = mapper.Map<List<ProductResponse?>>(result);
+
+        return response;
+    }
+
+    public async Task<List<ProductResponse?>> GetProductsByCondition(Expression<Func<Product, bool>> expression)
+    {
+        var result = await productsRepository.GetProductsByCondition(expression);
+
+        var response = mapper.Map<List<ProductResponse?>>(result);
+
+        return response;
+    }
+
+    public async Task<ProductResponse?> UpdateProduct(ProductUpdateRequest request)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        var validationResult = await updateValidator.ValidateAsync(request);
+
+        if (!validationResult.IsValid)
+        {
+            var errors = string.Join(",", validationResult.Errors.Select(x => x.ErrorMessage));
+
+            throw new ArgumentException(errors);
+        }
+
+        var product = mapper.Map<Product>(request);
+
+        var result = await productsRepository.UpdateProduct(product);
+
+        var response = mapper.Map<ProductResponse>(result);
+
+        return response;
+    }
+}
