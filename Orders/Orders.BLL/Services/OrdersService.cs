6782f50fa263e879ea6692c2cec7a3476e3dc985
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-04-05 21:06:48 +0300

    Orders service

---
+++ Orders/Orders.BLL/Services/OrdersService.cs
@@ -0,0 +1,73 @@
+using AutoMapper;
+using MongoDB.Driver;
+using Orders.BLL.DTO;
+using Orders.BLL.ServiceContracts;
+using Orders.DAL.Contracts;
+using Orders.DAL.Entities;
+
+namespace Orders.BLL.Services;
+
+public class OrdersService(
+    IOrdersRepository ordersRepository,
+    IMapper mapper) : IOrdersService
+{
+    public async Task<OrderResponse?> AddOrder(OrderAddRequest orderAddRequest)
+    {
+        // TODO validate UserID
+
+        var order = mapper.Map<Order>(orderAddRequest);
+
+        foreach (var item in order.OrderItems)
+        {
+            item.TotalPrice = item.Quantity * item.UnitPrice;
+        }
+
+        order.TotalBill = order.OrderItems.Sum(x => x.TotalPrice);
+
+        var result = mapper.Map<OrderResponse?>(await ordersRepository.AddOrder(order));
+
+        return result;
+    }
+
+    public async Task<bool> DeleteOrder(Guid orderID)
+    {
+        return await ordersRepository.DeleteOrder(orderID);
+    }
+
+    public async Task<OrderResponse?> GetOrderByCondition(FilterDefinition<Order> filter)
+    {
+        var result = mapper.Map<OrderResponse?>(await ordersRepository.GetOrderByCondition(filter));
+
+        return result;
+    }
+
+    public async Task<List<OrderResponse?>> GetOrders()
+    {
+        var result = mapper.Map<List<OrderResponse?>>(await ordersRepository.GetOrders());
+
+        return result;
+    }
+
+    public async Task<List<OrderResponse?>> GetOrdersByCondition(FilterDefinition<Order> filter)
+    {
+        var result = mapper.Map<List<OrderResponse?>>(await ordersRepository.GetOrdersByCondition(filter));
+
+        return result;
+    }
+
+    public async Task<OrderResponse?> UpdateOrder(OrderUpdateRequest orderUpdateRequest)
+    {
+        var order = mapper.Map<Order>(orderUpdateRequest);
+
+        foreach (var item in order.OrderItems)
+        {
+            item.TotalPrice = item.Quantity * item.UnitPrice;
+        }
+
+        order.TotalBill = order.OrderItems.Sum(x => x.TotalPrice);
+
+        var result = mapper.Map<OrderResponse?>(await ordersRepository.UpdateOrder(order));
+
+        return result;
+    }
+}
