ad597a8fc8811df502252fceaec64e6e9811ddc8
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-04-10 09:18:03 +0300

    Reading from cache

--- Orders/Orders.BLL/HttpClients/ProductsMicroserviceClient .cs
+++ Orders/Orders.BLL/HttpClients/ProductsMicroserviceClient .cs
@@ -1,12 +1,27 @@
 using System.Net.Http.Json;
+using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
 using Orders.BLL.DTO;
 
 namespace Orders.BLL.HttpClients;
 
-public class ProductsMicroserviceClient(HttpClient httpClient)
+public class ProductsMicroserviceClient(
+    HttpClient httpClient,
+    IDistributedCache cache)
 {
     public async Task<ProductDTO?> GetProductByProductID(Guid productID)
     {
+        var cacheKey = $"product:{productID}";
+
+        var cachedProduct = await cache.GetStringAsync(cacheKey);
+
+        if (cachedProduct is not null)
+        {
+            var productFromCache = JsonSerializer.Deserialize<ProductDTO>(cachedProduct);
+
+            return productFromCache;
+        }
+
         var result = await httpClient.GetAsync($"/api/products/search/product-id/{productID}");
 
         if (!result.IsSuccessStatusCode)
