1daf6f1a0a09154330a1940fe8446b66760c0eb9
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-04-12 08:44:36 +0300

    Setting up gateway

--- Orders/ApiGateway/Program.cs
+++ Orders/ApiGateway/Program.cs
@@ -2,10 +2,11 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot();
+
+var app = builder.Build();
 
 await app.UseOcelot();
 
