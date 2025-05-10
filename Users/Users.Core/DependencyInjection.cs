fb96563b96564bd5931dfe094db38c5548708f8d
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-05-10 08:56:53 +0300

    Setup azure graph api

--- Users/Users.Core/DependencyInjection.cs
+++ Users/Users.Core/DependencyInjection.cs
@@ -1,5 +1,7 @@
+using Azure.Identity;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Graph;
 using Users.Core.ServiceContracts;
 using Users.Core.Services;
 using Users.Core.Validators;
@@ -12,6 +14,18 @@ public static class DependencyInjection
     {
         services.AddTransient<IUsersService, UsersService>();
         services.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
+        services.AddScoped<GraphServiceClient>(provider =>
+        {
+            var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+            var options = new ClientSecretCredentialOptions() { AuthorityHost = AzureAuthorityHosts.AzurePublicCloud };
+
+            var clientSecretCredential = new ClientSecretCredential("tenant-id", "client-id", "client-secret", options);
+
+            var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+
+            return graphClient;
+        });
 
         return services;
     }
